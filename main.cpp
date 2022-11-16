ad8ba3bf07a197064ba7ffcee2f5f23af1500401
fedef14 <115482760+fedef14@users.noreply.github.com>
2022-11-16 11:27:46 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <math.h>
 using namespace std;
 
 int main() {
+   
+   float a = 0;
+   float b = 0;
+   float x = 0;
+   float y = 0;
+   float z = 0;
+   
+   cin >> a >> b >> x >> y;
+   
+   if(x<0 && y>0){
+   z = a*x-b*y;   
+   } else if(x>=0 && y<=0){
+   z = a*pow(x,2)-b*y;
+   } else{ 
+   z = a*x+b*pow(y,2);
+   }
+   
+   cout << z << endl;
+   
    return 0;
 }
